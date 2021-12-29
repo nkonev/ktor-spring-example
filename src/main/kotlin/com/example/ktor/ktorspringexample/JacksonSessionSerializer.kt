6a37ab891f31499eb9f328e893074c986f77e3a5
Nikita Konev <nikit.cpp@yandex.ru>
2021-12-30 01:15:21 +0300

    rm deprecation

--- src/main/kotlin/com/example/ktor/ktorspringexample/JacksonSessionSerializer.kt
+++ src/main/kotlin/com/example/ktor/ktorspringexample/JacksonSessionSerializer.kt
@@ -1,13 +1,21 @@
 package io.ktor.samples.sandbox
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import io.ktor.sessions.*
 
 class JacksonSessionSerializer<T>(
     private val type: Class<T>
 ): SessionSerializer<T> {
-    private val jackson = ObjectMapper().registerModule(KotlinModule())
+    private val jackson = ObjectMapper().registerModule(KotlinModule.Builder()
+            .withReflectionCacheSize(512)
+            .configure(KotlinFeature.NullToEmptyCollection, false)
+            .configure(KotlinFeature.NullToEmptyMap, false)
+            .configure(KotlinFeature.NullIsSameAsDefault, false)
+            .configure(KotlinFeature.SingletonSupport, false)
+            .configure(KotlinFeature.StrictNullChecks, false)
+            .build())
     override fun deserialize(text: String): T {
         return jackson.readValue(text, type) as T
     }
